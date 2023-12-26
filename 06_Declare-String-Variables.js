e79db1035a3e93d854ac516003f090e919f23056
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-12-26 20:55:24 +0100

    Declared string var

---
+++ 06_Declare-String-Variables.js
@@ -0,0 +1,7 @@
+/* "some characters" is called a string literal. 
+A string literal, or string, is a series of zero or 
+more characters enclosed in single or double quotes. */
+// Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+var myFirstName = 'Kseniia';
+var myLastName = 'Zhykina';
