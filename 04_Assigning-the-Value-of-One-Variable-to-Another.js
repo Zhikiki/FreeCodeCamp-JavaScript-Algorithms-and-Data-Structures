41b8ccb78e20d60bae265fb8efea80c8e8df17f3
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-12-26 20:15:16 +0100

    Assigne valueof var a to var b

---
+++ 04_Assigning-the-Value-of-One-Variable-to-Another.js
@@ -0,0 +1,11 @@
+/* After a value is assigned to a variable using the assignment operator, 
+you can assign the value of that variable to another variable 
+using the assignment operator. */
+// Assign the contents of a to variable b.
+
+// Setup
+var a;
+a = 7;
+var b;
+
+b = a;
