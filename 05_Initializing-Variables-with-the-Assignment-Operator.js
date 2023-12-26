39b512ae807b8255e32f385dfef2f73851730ce7
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-12-26 20:18:29 +0100

    Declared & initialised var in one line

---
+++ 05_Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,6 @@
+/* It is common to initialize a variable to an initial 
+value in the same line as it is declared. */
+// Define a variable a with var and initialize it to a value of 9.
+
+
+var a = 9;
