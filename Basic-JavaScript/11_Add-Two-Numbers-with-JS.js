238f545000a771a705ceacb0372714c75a611cc4
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-12-26 21:47:36 +0100

    Added two numbers with JS

---
+++ Basic-JavaScript/11_Add-Two-Numbers-with-JS.js
@@ -0,0 +1,11 @@
+/* Number is a data type in JavaScript which represents numeric data.
+JavaScript uses the + symbol as an addition operator when placed between two numbers.
+
+Example:
+
+const myVar = 5 + 10; */
+
+// Change the 0 so that sum will equal 20.
+// const sum = 10 + 0;
+
+const sum = 10 + 10;
