702165fededdc3f29d515202bcacca4e0dfdee3b
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-12-26 20:02:37 +0100

    Stored variable value

---
+++ 03_Storing-Variable-Values.js
@@ -0,0 +1,10 @@
+/* In JavaScript, you can store a value in a variable with the assignment operator (=). */
+/* If there are any calculations to the right of the = operator, 
+those are performed before the value is assigned to the variable 
+on the left of the operator */
+//Assign the value 7 to variable a.
+// Setup
+var a;
+
+// Only change code below this line
+a = 7;
