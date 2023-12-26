6789ed3e9d41bdaa1cf343a57cd9f635c08ad588
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-12-26 19:54:01 +0100

    Created variable

---
+++ 02_Declare-JavaScript-Variables.js
@@ -0,0 +1,14 @@
+/* JavaScript provides eight different data types which are 
+undefined, 
+null, 
+boolean, 
+string, 
+symbol, 
+bigint, 
+number, 
+object. */
+/* We tell JavaScript to create or declare a variable
+ by putting the keyword var in front of it.
+ In JavaScript we end statements with semicolons. */
+//Use the var keyword to create a variable called myName.
+var myName;
