67a03689175a9c604c57ee8544b159d80e3e0bf6
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2024-01-01 17:04:55 +0100

    Finished Profol Lookup Challenge

--- Basic-JavaScript/104_Profile-Lookup-Challenge.js
+++ Basic-JavaScript/104_Profile-Lookup-Challenge.js
@@ -1,4 +1,21 @@
 /*
+We have an array of objects representing different people in 
+our contacts lists.
+
+A lookUpProfile function that takes name and a property (prop) 
+as arguments has been pre-written for you.
+
+The function should check if name is an actual contact's 
+firstName and the given property (prop) is a property of that contact.
+
+If both are true, then return the "value" of that property.
+
+If name does not correspond to any contacts then return the 
+string No such contact.
+
+If prop does not correspond to any valid properties of a contact 
+found to match name then return the string No such property.
+
 
 */
 
@@ -7,4 +24,54 @@
 */
 
 // Setup
+const contacts = [
+  {
+    firstName: 'Akira',
+    lastName: 'Laine',
+    number: '0543236543',
+    likes: ['Pizza', 'Coding', 'Brownie Points'],
+  },
+  {
+    firstName: 'Harry',
+    lastName: 'Potter',
+    number: '0994372684',
+    likes: ['Hogwarts', 'Magic', 'Hagrid'],
+  },
+  {
+    firstName: 'Sherlock',
+    lastName: 'Holmes',
+    number: '0487345643',
+    likes: ['Intriguing Cases', 'Violin'],
+  },
+  {
+    firstName: 'Kristian',
+    lastName: 'Vos',
+    number: 'unknown',
+    likes: ['JavaScript', 'Gaming', 'Foxes'],
+  },
+];
 
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (let i = 0; i < contacts.length; i++) {
+    for (let i = 0; i < contacts.length; i++) {
+      if (contacts[i].firstName === name) {
+        if (contacts[i].hasOwnProperty(prop)) {
+          return contacts[i][prop];
+        } else {
+          return 'No such property';
+        }
+      }
+    }
+    return 'No such contact';
+  }
+  // Only change code above this line
+}
+
+lookUpProfile('Akira', 'likes'); // "Pizza", "Coding", "Brownie Points"
+console.log(lookUpProfile('Kristian', 'lastName')); // Vos
+console.log(lookUpProfile('Sherlock', 'likes')); // ["Intriguing Cases", "Violin"]
+console.log(lookUpProfile('Harry', 'likes')); //"Hogwarts", "Magic", "Hagrid"
+console.log(lookUpProfile('Bob', 'number')); // No such contact
+console.log(lookUpProfile('Bob', 'potato')); // No such contact
+console.log(lookUpProfile('Akira', 'address')); // No such property
